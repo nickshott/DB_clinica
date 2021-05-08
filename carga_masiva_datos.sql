dbc5f0d641066a544375926fec7e2ebfa3a88dff
nickshott <46877011+nickshott@users.noreply.github.com>
2021-05-08 15:28:57 -0500

    Create carga_masiva_datos.sql

---
+++ carga_masiva_datos.sql
@@ -0,0 +1,15 @@
+copy PUBLIC."id_tipo_med"
+from 'C:\Users\BETO\Desktop\manejo bases de datos/tipo_med.txt'
+delimiter ';'
+
+copy PUBLIC."Pacientes"
+from 'C:\Users\BETO\Desktop\manejo bases de datos/registro_pacientes.txt'
+delimiter ';'
+
+copy PUBLIC."Diagnostico"
+from 'C:\Users\BETO\Desktop\manejo bases de datos/diagnosis_tipo.txt'
+delimiter ';'
+
+copy PUBLIC."Registro_med"
+from 'C:\Users\BETO\Desktop\manejo bases de datos/registro_consumo.txt'
+delimiter ';'
